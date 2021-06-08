ff02392b85df0a49657690e83149fa88250fab28
Robin <rd.intermac@googlemail.com>
2021-06-08 12:22:16 +0200

    fixed blog pagination problem

--- index.php
+++ index.php
@@ -1,23 +1,13 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<?php 
-  //the query
-  $the_query = new WP_Query(array(
-    'category_name' => 'blog,blog-2'
-  ));
-?>
-
-<?php if ($the_query->have_posts()) : ?>
-
-<?php $count = 1; while ($the_query->have_posts()) : $the_query->the_post(); $count++; ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
-
-<?php else: ?>
-
-<div class="alert alert-warning">
-    <?php _e('Tut uns leid – hier liegt anscheinend ein Fehler vor', 'sage'); ?>
+<?php if (!have_posts()) : ?>
+  <div class="alert alert-warning">
+  <?php _e('Tut uns leid – hier liegt anscheinend ein Fehler vor', 'sage'); ?>
   </div>
 <?php endif; ?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+<?php endwhile; ?>
 
 <?php the_posts_navigation(); ?>
